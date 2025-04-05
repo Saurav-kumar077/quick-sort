22d94785806762af115d579b5e968ff98075d8df
Saurav-kumar077 <126255878+Saurav-kumar077@users.noreply.github.com>
2025-04-05 09:57:14 +0530

    updated

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,60 +1,68 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int partition(int arr[],int s,int e){
-    int pivot = arr[s];
 
-    int count =0;
-    for(int i=s+1;i<=e;i++){
-        if(arr[i]<=pivot){
-            count ++;
-        }
+int partition(int arr[], int s, int e)
+{
+  int pivot = arr[s];
+  int count = 0;
 
-    }
-    int pivotIndex = s + count;
-    swap(arr[pivotIndex],arr[s]);
-
-    int i =s;
-    int j =e;
-
-    while (i<pivotIndex && j>pivotIndex)
+  for (int i = s + 1; i <= e; i++)
+  {
+    if (arr[i] <= pivot)
     {
-      while(arr[i]<= pivot)  {
-        i++;
-      }
-      while(arr[j]>=pivot){
-        j--;
-      }
-
-      if(i< pivotIndex && j> pivotIndex){
-        swap(arr[i++],arr[j--]);
-      }
-      
+      count++;
     }
-    return pivotIndex;
-    
-}
-void quickSort(int arr[],int s, int e){
-    if(s>=e){
-        return;
+  }
+
+  int pivotIndex = s + count;
+  swap(arr[pivotIndex], arr[s]);
+
+  int i = s, j = e;
+
+  while (i < pivotIndex && j > pivotIndex)
+  {
+    while (i < pivotIndex && arr[i] <= pivot)
+    {
+      i++;
     }
-    int p = partition(arr,s,e);
+    while (j > pivotIndex && arr[j] > pivot)
+    {
+      j--;
+    }
 
-        quickSort(arr,s,p-1);
-        quickSort(arr,p+1,e);
-    
+    if (i < pivotIndex && j > pivotIndex)
+    {
+      swap(arr[i++], arr[j--]);
+    }
+  }
 
+  return pivotIndex;
 }
-int main(){
-    int arr[7]={3,5,1,8,2,4,6};
-    int size = 7;
-    quickSort(arr,0,size-1);
-    for(int i=0;i<size;i++){
-        cout<<arr[i]<< " ";
 
+void quickSort(int arr[], int s, int e)
+{
+  if (s >= e)
+  {
+    return;
+  }
 
-    }cout<<endl;
+  int p = partition(arr, s, e);
+  quickSort(arr, s, p - 1);
+  quickSort(arr, p + 1, e);
+}
 
+int main()
+{
+  int arr[10] = {3, 5, 1, 8, 8, 2, 4, 6, 6, 6};
+  int size = 10;
 
+  quickSort(arr, 0, size - 1);
 
-    return 0;
+  for (int i = 0; i < size; i++)
+  {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+
+  return 0;
 }
